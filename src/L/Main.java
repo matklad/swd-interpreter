401a2d158c4ce76a19f74cbf8a575a0bc4be60f0
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-12-01 16:47:01 +0300

    print statistics in Main

--- src/L/Main.java
+++ src/L/Main.java
@@ -1,6 +1,7 @@
 package L;
 
 import L.ast.Program;
+import L.statistic.Collectors;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,6 +12,7 @@ public final class Main {
     byte[] bytes = Files.readAllBytes(Paths.get(args[0]));
     String input = new String(bytes, "UTF-8");
     Program p = Parser.parseProgram(input);
+    System.out.println("Program has " + Collectors.numberOfAdditions(p) + " additions");
     Evaluator.traceEval(p).forEach(System.out::println);
   }
 }
